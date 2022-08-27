dea9586db2a5e8954ee7b5971998590ed3102bf3
Alex <nike95125@gmail.com>
2022-08-28 00:21:56 +0300

    added banner-slider section

---
+++ app/js/main.js
@@ -0,0 +1,6 @@
+const bannerSlider = new Slider({
+    slideClass: 'banner-item',
+    btnClass: 'banner__btn',
+    btnClassInner: 'banner__btn-inner',
+    counterClass: 'banner__counter',
+});
--- gulpfile.js
+++ gulpfile.js
@@ -71,6 +71,7 @@ function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
     'node_modules/slick-carousel/slick/slick.js',
+    'app/js/slider.js',
     'app/js/main.js',
   ])
   .pipe(concat('main.min.js'))
